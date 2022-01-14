9ea63492f4f6f694775af307d67ef84735180d46
Vulombe <vmakhubele@gmail.com>
2022-01-14 14:49:00 +0200

    Everyone's first commit

---
+++ src/java/za/co/bakery/controllers/PatCakeControl.java
@@ -0,0 +1,102 @@
+package za.co.bakery.controllers;
+
+import za.co.bakery.service.UserService;
+import za.co.bakery.service.UserServiceImpl;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PatCakeControl extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String prs = request.getParameter("pro");
+        if (prs != null) {
+            if (prs.equals("login")) {
+                UserService us = new UserServiceImpl();
+                Boolean valid = us.isUserValid("flib@flob", "password");
+                request.setAttribute("valid", valid);
+                RequestDispatcher view = request.getRequestDispatcher("loginAnswer.jsp");
+                view.forward(request, response);
+            }
+//        
+//        response.setContentType("text/html;charset=UTF-8");
+//        try (PrintWriter out = response.getWriter()) {
+//            /* TODO output your page here. You may use following sample code. */
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet PatCakeControl</title>");
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>Servlet PatCakeControl at " + request.getContextPath() + "</h1>");
+//            String prs = request.getParameter("pro");
+//            if (prs != null) {
+//                if (prs.equals("login")) {
+//                    UserService us = new UserServiceImpl();
+//                    out.println("<h1>UserValid?  " + us.isUserValid("flib@flob", "password") + "</h1>");
+//                }
+//            }
+//            out.println("</body>");
+//            out.println("</html>");
+//            
+        }
+//        
+            }
+
+            // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+            /**
+             * Handles the HTTP <code>GET</code> method.
+             *
+             * @param request servlet request
+             * @param response servlet response
+             * @throws ServletException if a servlet-specific error occurs
+             * @throws IOException if an I/O error occurs
+             */
+            @Override
+            protected void doGet
+            (HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+                processRequest(request, response);
+            }
+
+            /**
+             * Handles the HTTP <code>POST</code> method.
+             *
+             * @param request servlet request
+             * @param response servlet response
+             * @throws ServletException if a servlet-specific error occurs
+             * @throws IOException if an I/O error occurs
+             */
+            @Override
+            protected void doPost
+            (HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+                processRequest(request, response);
+            }
+
+            /**
+             * Returns a short description of the servlet.
+             *
+             * @return a String containing servlet description
+             */
+            @Override
+            public String getServletInfo
+            
+                () {
+        return "Short description";
+            }// </editor-fold>
+
+        }
---
+++ src/java/za/co/bakery/domain/Product.java
@@ -0,0 +1,114 @@
+package za.co.bakery.domain;
+
+/**
+ *
+ * @author StuartLittles
+ */
+public class Product {
+    private String picture;
+    private double price;
+    private Category category;
+    private String warning;
+    private String description;
+    private Recipe recipe;
+    private int productID;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getWarning() {
+        return warning;
+    }
+
+    public void setWarning(String warning) {
+        this.warning = warning;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 59 * hash + this.productID;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Product other = (Product) obj;
+        if (this.productID != other.productID) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
